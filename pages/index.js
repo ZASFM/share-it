91c7fedbded9292d5970c43440a932001a03a643
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-04-06 19:28:40 +0200

    inital setup

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import '../styles/Home.module.css';
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+     <div className='container mx-auto px-10 mb-8'>
+        <Head>
+          <title>Blog</title>
+          <link rel='icon' href='/favicon.icon'/>
+        </Head>
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+            
+        </div>
+     </div>
+  )
+}
