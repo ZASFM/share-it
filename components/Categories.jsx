afc537e96cc542eb3fd6efc72d2d6445c341eaa2
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-04-12 14:36:57 +0200

    sides, all sides

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,29 @@
-const Categories=()=>{
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { getCategories } from "../services/index";
+
+const Categories = () => {
+   const [categories, setCategories] = useState([]);
+
+   useEffect(() => {
+      getCategories()
+         .then(categories => setCategories(categories));
+   }, [])
+
    return (
-      <div>
-         Categories
+      <div className="bg-white shadow-lg rounded-lg p-8 mb-8 ">
+         <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+            Categories
+         </h3>
+         {
+            categories.map(category=>(
+               <Link href={`/category/${category.slug}`} key={category.slug}>
+                  <span className="cursor-pointer block pb-3 mb-3">
+                    {category.name}
+                  </span>
+               </Link>
+            ))
+         }
       </div>
    )
 }
